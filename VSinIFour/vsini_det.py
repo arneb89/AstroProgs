bfa781ef3ace422e29438c78402da47c2dc38c80
arneb89 <kolbinalexander@mail.ru>
2017-08-25 18:31:28 +0300

    Some fixes

--- VSinIFour/vsini_det.py
+++ VSinIFour/vsini_det.py
@@ -1,15 +1,35 @@
 import numpy as np
 from scipy.interpolate import LSQUnivariateSpline
 import matplotlib.pyplot as plt
-prof = np.loadtxt("aver_373.lsd")
+
+
+
+prof = np.loadtxt("20131027_02_comp1.lsd")
+bb1=-138 # left bound of the line profile;
+bb2=-35  # right bound of the line profile;
+bound_inten=0.98779
+
+
 xx = prof[:,0]
 yy = prof[:,1]
-plt.plot(xx, yy)
 dx=xx[1]-xx[0]
 
-t = np.linspace(-200, 200, 8)
+# crop the spectrum
+k=0
+for i in range(len(xx)):
+    if xx[i]>=bb1 and xx[i]<=bb2:
+        xx[k]=xx[i]
+        yy[k]=yy[i]
+        k=k+1
+xx=xx[0:k]
+yy=yy[0:k]
+
+plt.subplot(211)
+plt.plot(xx, yy)    
+
+t = np.linspace(bb1+10, bb2-10, 8)
 spl = LSQUnivariateSpline(xx, yy, t)
-xx_mod=np.linspace(-250, 250, 100)
+xx_mod=np.linspace(bb1, bb2, 100)
 yy_mod = spl(xx_mod)
 plt.plot(xx_mod, yy_mod)
 std=0
@@ -27,17 +47,18 @@ yys = np.zeros([n_samples, len(yy)])
 for i in range(0, n_samples):
     for j in range(0, len(xx)):
         yys[i,j]=spl(xx[j])+np.random.normal(0, std)
-    #plt.plot(xx, yys[i,:])
+    plt.plot(xx, yys[i,:])
     
-plt.show()
 
-yys1=np.zeros([n_samples, len(xx)+6000])
+yys1=np.zeros([n_samples, len(xx)+4000])
 for k in range(0, n_samples):
     for i in range(0, len(yys1[k,:])):
-        yys1[k,i]=1.0
+        yys1[k,i]=bound_inten
     for i in range(0, len(xx)):
-        yys1[k,i+3000]=yys[k, i]
+        yys1[k,i+2000]=yys[k, i]
 
+
+plt.subplot(212)
 
 nu = np.fft.fftfreq(len(yys1[0,:]), dx)
 nu = np.fft.fftshift(nu)
@@ -48,8 +69,8 @@ for k in range(0, n_samples):
     ff = np.absolute(ff)**2
     plt.plot(nu, np.log(ff), '-')
     
-    ff_lim_1=0.003
-    ff_lim_2=0.005
+    ff_lim_1=0.016
+    ff_lim_2=0.023
     mini = 1e30
     mini_x = 0
     for i in range(0, len(nu)):
@@ -59,7 +80,7 @@ for k in range(0, n_samples):
                 mini_x=nu[i]
     
     min_ff[k]=mini_x
-plt.xlim(0,0.01)
+plt.xlim(0,0.05)
 plt.show()
     
 print(min_ff)
